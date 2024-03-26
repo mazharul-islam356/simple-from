76ba5f9c2464371ef6c54f01db2d1586fd067a0a
rifat3568 <mazharulislam3569@gmail.com>
2024-03-26 22:38:27 +0600

    add validation

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
-const yesNoRadios = document.querySelectorAll('input[type="radio"]');
-const yesInputs = document.querySelectorAll('input[required]');
+document.addEventListener("DOMContentLoaded", function() {
+  const yesRadio = document.getElementById("yesRadio");
+  const yesRadio2 = document.getElementById("yesRadio2");
+  
+  const myInput = document.getElementById('myInput');
+  // Set required to true (optional, defaults to false)
+  myInput.required = true;
+  const myInput2 = document.getElementById('myInput2');
+  // Set required to true (optional, defaults to false)
+  myInput.required = true;
+  myInput2.required = true;
 
-yesNoRadios.forEach(radio => {
-  radio.addEventListener('change', () => {
-    const nextInput = radio.nextElementSibling.nextElementSibling; // Get the next input after the label
-    if (radio.checked && radio.value === 'YES') {
-      nextInput.value('required');
+
+  yesRadio.addEventListener("change", function() {
+    if (this.checked) {
+      myInput.required = true;
     }
   });
+
+  document.getElementById("noRadio").addEventListener("change", function() {
+    myInput.required = false;
+  });
+
+
+  // -------------------2nd input-------------
+  yesRadio2.addEventListener("change", function() {
+    if (this.checked) {
+      myInput2.required = true;
+    }
+  });
+
+  document.getElementById("noRadio2").addEventListener("change", function() {
+    myInput2.required = false;
+  });
+
+
 });
+
+
+
+const phoneInputField = document.getElementById('phone_number');
+intlTelInput(phoneInputField, {
+  initialCountry: "ca",
+  utilsScript: "https://github.com/topics/intl-tel-input" })
+
+
+  
+ 
+
+
+
+
+
+
+
